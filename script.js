9580b8d9cb19a033b0a13a547ffceebc24e67886
antiluse <165805853+antiluse@users.noreply.github.com>
2026-01-15 08:46:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,20 +2,29 @@ const sideMenu = document.getElementById("sideMenu");
 const overlay = document.getElementById("overlay");
 const popup = document.getElementById("successPopup");
 
-function openMenu() {
-    sideMenu.classList.add("open");
-    overlay.classList.add("show");
+function openMenu(){
+sideMenu.classList.add("open");
+overlay.classList.add("show");
+}
+function closeMenu(){
+sideMenu.classList.remove("open");
+overlay.classList.remove("show");
 }
 
-function closeMenu() {
-    sideMenu.classList.remove("open");
-    overlay.classList.remove("show");
-}
-
-document.getElementById("bookingForm")
-    .addEventListener("submit", function (e) {
-    e.preventDefault();
-    popup.style.display = "flex";
+document.getElementById("bookingForm").addEventListener("submit",e=>{
+e.preventDefault();
+popup.style.display="flex";
 });
 
-function closePopup() { popup.style.display = "none"; }
+function closePopup(){
+popup.style.display="none";
+}
+
+const reveals=document.querySelectorAll(".reveal");
+window.addEventListener("scroll",()=>{
+reveals.forEach(el=>{
+if(el.getBoundingClientRect().top<window.innerHeight-100){
+el.classList.add("active")
+}})
+});
+
