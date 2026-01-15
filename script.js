7ab1c5eaf1c0fae720794dab60cb661dddf50fdb
antiluse <165805853+antiluse@users.noreply.github.com>
2026-01-15 08:36:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
+const sideMenu = document.getElementById("sideMenu");
+const overlay = document.getElementById("overlay");
+const popup = document.getElementById("successPopup");
+
 function openMenu() {
-    sideMenu.classList.add('open');
-    overlay.classList.add('show');
+    sideMenu.classList.add("open");
+    overlay.classList.add("show");
 }
 
 function closeMenu() {
-    sideMenu.classList.remove('open');
-    overlay.classList.remove('show');
+    sideMenu.classList.remove("open");
+    overlay.classList.remove("show");
 }
 
-const sideMenu = document.getElementById('sideMenu');
-const overlay = document.getElementById('overlay');
+document.getElementById("bookingForm")
+    .addEventListener("submit", function (e) {
+    e.preventDefault();
+    popup.style.display = "flex";
+});
 
-const reveals = document.querySelectorAll('.reveal');
-
-function revealOnScroll() {
-    reveals.forEach(el => {
-        if (el.getBoundingClientRect().top < window.innerHeight - 100) {
-            el.classList.add('active');
-        }
-    });
-}
-
-window.addEventListener('scroll', revealOnScroll);
-revealOnScroll();
+function closePopup() { popup.style.display = "none"; }
